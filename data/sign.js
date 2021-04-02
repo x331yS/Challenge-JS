f1a2175ba1015731b6dd34ddd2e4fcfb54a313c3
FLEJOSNE <florian.lejosne@ynov.com>
2021-04-02 12:14:18 +0200

    [ADD] new file

---
+++ data/sign.js
@@ -0,0 +1,17 @@
+function sign(num) {
+    if (num > 0) {
+        return 1
+
+    }
+    if (num === 0) {
+        return 0
+
+    }
+    return -1
+}
+function sameSign(num1, num2){
+    if (num1 === num2){
+        return true
+    }
+    return false
+}

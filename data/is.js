1ba28fc51edff6dcec0e0bc3e6736540e8685473
FLEJOSNE <florian.lejosne@ynov.com>
2021-04-02 12:43:51 +0200

    [ADD] new file

--- data/is.js
+++ data/is.js
@@ -1,14 +1,15 @@
 const is = {}
-is.num = (typ) => typeof typ === 'number'
-is.nan = (typ) => Number.isNaN(typ)
+
+is.num = (arg) => typeof(arg) === 'number' ? typeof(arg) : ""
+is.nan = (arg) => Number.isNaN(arg)
 is.str = (arg) => typeof(arg) === 'string' ? typeof(arg) : ""
 is.bool = (arg) => typeof(arg) === 'boolean' ? typeof(arg) : ""
 is.undef = (arg) => typeof arg === 'undefined' ? true : ""
 is.def = (arg) => typeof arg !== 'undefined' ? true : ""
-is.arr = (typ) => arr.isArray()
+is.arr = (arg) => toString.call(arg).slice(8, -1) === 'Array' ? toString.call(arg).slice(8, -1) : ""
 is.obj = (arg) => typeof(arg) === 'object' ? toString.call(arg).slice(8, -1) === 'Array' ? "" : arg === null ? "" : typeof arg:  ""
-is.fun = (typ) => typeof(typ) === 'function' ? typeof(typ) : ""
+is.fun = (arg) => typeof(arg) === 'function' ? typeof(arg) : ""
 is.truthy = (arg) => (arg) ? true : ""
-is.falsy  = (typ) =>  typ === false
+is.falsy = (arg) => (!arg) ? true : ""
 
 console.log(is.undef(NaN))

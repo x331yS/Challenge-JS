c1212aa11381c8c59bdb3ea63d4cf561b3ab58f0
FLEJOSNE <florian.lejosne@ynov.com>
2021-04-02 12:35:21 +0200

    [ADD] new file

--- data/is.js
+++ data/is.js
@@ -5,5 +5,7 @@ is.bool = (typ) => typeof typ === 'boolean'
 is.undef = (typ) => typeof typ === 'undefined'
 is.def = (typ) => typeof typ !== 'undefined'
 is.arr = (typ) => arr.isArray()
+is.obj = (arg) => typeof(arg) === 'object' ? toString.call(arg).slice(8, -1) === 'Array' ? "" : arg === null ? "" : typeof arg:  ""
+is.fun = (typ) => typeof(typ) === 'function' ? typeof(typ) : ""
 is.truthy = (typ) =>  typ === true
 is.falsy  = (typ) =>  typ === false

4f7fc706175fb303dc5c64454f6c938e6aa1cbd8
FLEJOSNE <florian.lejosne@ynov.com>
2021-04-02 11:57:53 +0200

    [ADD] new file

---
+++ abs.js
@@ -0,0 +1,10 @@
+function isPositive(number){
+    return number < 0
+}
+function abs(number) {
+    if (isPositive(number))  {
+        return -number
+    }
+    return number
+}
+console.log(abs(0))

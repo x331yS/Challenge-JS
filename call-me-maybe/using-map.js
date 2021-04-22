5a10dc4ba5397ceda1f143f456e629ba609edbc4
FLEJOSNE <florian.lejosne@ynov.com>
2021-04-22 08:57:37 +0200

    [ADD] file

---
+++ call-me-maybe/using-map.js
@@ -0,0 +1,21 @@
+const citiesOnly = (arr) => arr.map(x => x['city'])
+const toUpper = string => string.replace(/(^\w{1})|(\s{1}\w{1})/g, match => match.toUpperCase());
+
+function upperCasingStates(arr) {
+    return arr.map(function (string) {
+        let arr = string.split(" ");
+        return arr
+            .map(function (string) {
+                return string.charAt(0).toUpperCase() + string.slice(1);
+            })
+            .join(" ");
+    });
+}
+
+const fahrenheitToCelsius = (arr) => arr.map(str => Math.floor(((str.split("°F")[0] - 32) * 5) / 9) + "°C");
+
+function trimTemp(arr) {
+    return arr.map(abcdef => Object.assign(abcdef, {temperature: abcdef['temperature'].replace(/\s+/g, '')}))
+}
+
+const tempForecasts = (objArray) => objArray.map(obj => fahrenheitToCelsius(obj.temperature.replace(/ /g, '').split('°F')[0]) + '°Celsius in ' + obj.city + ', ' + toUpper(obj.state))

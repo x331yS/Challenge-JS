478900b98c2ec098a57e4b8b3c3efee0cb1eaac8
FLEJOSNE <florian.lejosne@ynov.com>
2021-04-23 08:49:35 +0200

    [ADD] file

---
+++ call-me-maybe/sweet-curry.js
@@ -0,0 +1,5 @@
+const mult2 = (num1) => (num2) => num1 * num2;
+
+const add3 = (num1) => (num2) => (num3) => num1 + num2 + num3;
+
+const sub4 = (num1) => (num2) => (num3) => (num4) => num1 - num2 - num3 - num4;

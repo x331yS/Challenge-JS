efca610af4d649d8501a175ebd15093a79729314
FLEJOSNE <florian.lejosne@ynov.com>
2021-04-28 14:38:03 +0200

    [ADD] file

--- dom/where-do-we-go.js
+++ dom/where-do-we-go.js
@@ -1,84 +1,98 @@
 import { places } from './where-do-we-go.data.js'
 
-export { explore }
+export const explore = () => {
+    let p = sortPlaces(places);
 
-function explore() {
-    let sortedPlaces = sort(places)
-    document.body.style.overflowX = 'hidden';
+    createHeading();
+    createDirection();
 
-    createSections(sortedPlaces)
-    createDirection()
+    let i = Math.round(window.scrollY / window.innerHeight);
+    let currentPlace;
 
-    let sections = document.querySelectorAll('section');
-    let direction = document.querySelector('.direction')
+    const setCurrentPlace = (index) => {
+        currentPlace = p[index];
+        refreshHeading(currentPlace);
+    };
 
-    const makeTag = (i) => {
-        const tag = document.createElement('a');
-        tag.className = 'location';
-        tag.style.color = sortedPlaces[i].color;
-        tag.textContent = `${sortedPlaces[i].name}\n${sortedPlaces[i].coordinates}`;
-        tag.setAttribute('href', `https://google.com/maps/place/${sortedPlaces[i].coordinates}`);
-        tag.setAttribute('target', '_blank');
-        sections[i].append(tag);
-    }
+    setCurrentPlace(i);
 
-    let prev = scrollY;
-    let prevIndex = Math.round(scrollY / window.innerHeight);
-    makeTag(prevIndex);
+    let lastPos = window.scrollY;
+    window.addEventListener("scroll", (e) => {
+        let index = Math.round(window.scrollY / window.innerHeight);
+        setCurrentPlace(index);
 
-    document.addEventListener('scroll', (e) => {
-        const index = Math.round(scrollY / window.innerHeight);
-        if (prevIndex != index) {
-            sections[prevIndex].childNodes[0].remove();
-            console.log(sections[index].childNodes, prevIndex, index)
-            makeTag(index);
+        if (lastPos < scrollY) {
+            document.querySelector(".direction").textContent = "S";
+        } else {
+            document.querySelector(".direction").textContent = "N";
         }
-        prevIndex = index;
 
-        if (prev > scrollY) direction.textContent = 'N';
-        else if (prev < scrollY) direction.textContent = 'S';
-        prev = scrollY;
+        lastPos = scrollY;
     });
-}
 
-function sort(arr) {
-    let array = [...arr]
-    array.sort((a, b) => {
-        if (getNorth(a) > getNorth(b)) {
-            return -1
-        }
-        if (getNorth(a) < getNorth(b)) {
-            return 1
-        }
-        return 0
-    })
-    return array
-}
+    renderSections(p);
+};
 
-function getNorth(obj) {
-    let north = obj.coordinates.split(' ')[0].replace(/[°'."]/g, '')
-    if (north.includes('N')) {
-        north = Number(north.slice(0, 5))
-    } else {
-        north = Number(north.slice(0, 5)) * -1
-    }
-    return north
-}
+const createDirection = () => {
+    let el = document.createElement("div");
+    el.classList.add("direction");
+    el.textContent = "S";
+    document.body.append(el);
+};
 
-function createSections(sortedPlaces) {
-    sortedPlaces.forEach((place) => {
-        let section = document.createElement('section');
-        section.style.background = `url(./images/${place.name.toLowerCase().split(',')[0].replace(/ /g, '-')}.jpg)`;
-        section.style.backgroundRepeat = 'no-repeat';
-        section.style.backgroundSize = 'cover';
-        section.style.backgroundPosition = 'center';
-        document.body.append(section);
+const refreshHeading = (p) => {
+    let h = document.querySelector("a.location");
+    h.textContent = `${p.name}\n${p.coordinates}`;
+    h.style.color = p.color;
+    h.setAttribute("href", `https://www.google.com/maps/place/${p.coordinates}`);
+};
+
+const createHeading = () => {
+    let el = document.createElement("a");
+    el.classList.add("location");
+    el.setAttribute("target", "_blank");
+    el.setAttribute("href", "#");
+    document.body.append(el);
+};
+
+const renderSections = (p) => {
+    p.forEach((q) => {
+        document.body.innerHTML += `<section style="background: url('./where-do-we-go_images/${q.name
+            .split(",")[0]
+            .toLowerCase()
+            .replace(/[ ]/g, "-")}.jpg');"></section>`;
     });
-}
+};
 
-function createDirection() {
-    let direction = document.createElement('div')
-    direction.className = 'direction';
-    direction.textContent = 'N';
-    document.body.append(direction);
-}
+const sortPlaces = (p) =>
+    p.sort((a, b) => {
+        let [lta] = DMS(a.coordinates);
+        let [ltb] = DMS(b.coordinates);
+
+        console.log(lta, ltb);
+
+        return ltb - lta;
+    });
+
+const DMS = (p) => {
+    let [lat, long] = p.split(" ");
+
+    let lt = DMShelper(lat.slice(0, -1));
+    let ln = DMShelper(long.slice(0, -1));
+
+    if (lat.slice(-1) === "S") lt = -lt;
+    if (lat.slice(-1) === "W") ln = -ln;
+
+    return [lt, ln];
+};
+
+const DMShelper = (lat) => {
+    let deg = +lat.slice(0, lat.indexOf("°"));
+    lat = lat.slice(lat.indexOf("°") + 1);
+
+    let min = +lat.slice(0, lat.indexOf("'"));
+    lat = lat.slice(lat.indexOf("'") + 1);
+    let sec = +lat.slice(0, -1);
+
+    return deg + min / 60 + sec / 60 / 60;
+};
